ed25a0aeb22d342d6b234693cf03d9bdd660e6bd
guohub8080 <guo2018@88.com>
2025-04-11 11:18:31 +0800

    bug fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,45 +3,44 @@ import vue from '@vitejs/plugin-vue'
 import {resolve} from "path"
 
 
-
 // https://vitejs.dev/config/
 
 export default defineConfig({
-    plugins: [vue(),
+	plugins: [vue(),
 
-    ],
-    // 静态资源加载路径：
+	],
+	// 静态资源加载路径：
 
-    resolve: {
-        alias: {
-            "@": resolve(__dirname, "src"),
-            path: "path-browserify",
-        }
-    },
-    css: {
-        // css预处理器
-        preprocessorOptions: {
-            less: {
-                javascriptEnable: true,
-                charset: false,
-                additionalData: '@import "./src/assets/global.less";',
-            },
-        },
-    },
-    base: process.env.NODE_ENV === 'production' ? '/musickit_v1/' : './',
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "src"),
+			path: "path-browserify",
+		}
+	},
+	css: {
+		// css预处理器
+		preprocessorOptions: {
+			less: {
+				javascriptEnable: true,
+				charset: false,
+				additionalData: '@import "./src/assets/global.less";',
+			},
+		},
+	},
+	base: './',
 
 
-    build: {
-        rollupOptions: {
-            output: {
-                chunkFileNames: "js/[name]-[hash].js",
-                entryFileNames: "js/[name]-[hash].js",
-                assetFileNames: "[ext]/[name]-[hash].[ext]",
-            }
-        },
-        outDir: "docs",
-        commonjsOptions: {
-            exclude: ['ckeditor/*'],
-        },
-    },
+	build: {
+		rollupOptions: {
+			output: {
+				chunkFileNames: "js/[name]-[hash].js",
+				entryFileNames: "js/[name]-[hash].js",
+				assetFileNames: "[ext]/[name]-[hash].[ext]",
+			}
+		},
+		outDir: "docs",
+		commonjsOptions: {
+			exclude: ['ckeditor/*'],
+		},
+	},
 })
